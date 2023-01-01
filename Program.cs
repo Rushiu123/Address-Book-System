ca269d7dab92b50f3d2dd0b41cab84104e022eb6
Rushiu123 <rushikesh.ubalkar@gmail.com>
2023-01-01 16:40:52 +0530

    UC.5,6 AddressBookSystem

--- Program.cs
+++ Program.cs
@@ -7,48 +7,44 @@
             Console.WriteLine("=================================\nWelcome" +
                 "To Address Book System\n=============================== ");
             Console.WriteLine("\n1. Create Contact\n");
-            Console.WriteLine("\n1. Add New Contact.\n");
+            Console.WriteLine("\n2. Add New Contact.\n3.Edit Contact.\n4.Delete Contact.\n5.Add Mualtiple Contact\n6.Multiple Address Book \n");
             int option = Convert.ToInt32(Console.ReadLine());
+
             switch (option)
             {
                 //UC1 - Create Contact
-                //UC1 - Add a New Contact
                 case 1:
-                    CreateContacts();
-                    AddressBook.CreateContacts();
+                    addressBook.CreateContacts();
+
+                case 1:
+                    addressBook.CreateContacts();
+                    break;
+                //UC2 - Add Contact and Display
+                case 2:
+                    addressBook.DisplayContact();
+                    break;
+                //UC3 - Exit Existing Contact
+                case 3:
+                    addressBook.EditContact();
+                    break;
+                case 4:
+                    //Uc4- Delete Contact
+                    addressBook.DeleteContact();
+                    break;
+                case 5:
+                    //Uc6- Add Address Book
+                    addressBook.AddNewAddressBook();
                     break;
                 default:
                     Console.WriteLine("Choose from given option only!\n");
                     break;
             }
         }
-        public static void CreateContacts()
-        {
-            //reading data and storing in set method/properties 
-            Console.Write("Enter First Name : ");
-            string firstName = Console.ReadLine();
-            //Last Name 
-            Console.Write("Enter Last Name : ");
-            string lastName = Console.ReadLine();
-            //Address
-            Console.Write("Enter Address : ");
-            string address = Console.ReadLine();
-            //City
-            Console.Write("Enter City Name : ");
-            string city = Console.ReadLine();
-            //State Name 
-            Console.Write("Enter State Name : ");
-            string state = Console.ReadLine();
-            //ZIP Code
-            Console.Write("Enter ZIP Code : ");
-            int ZIPCode = Convert.ToInt32(Console.ReadLine());
-            //Phone Number
-            Console.Write("Enter Phone Number : ");
-            long phoneNumber = Convert.ToInt64(Console.ReadLine());
-            //Email Id :
-            Console.Write("Enter Email Id : ");
-            string email = Console.ReadLine();
-        }
+
+    }
+}
+       
+        
     }
 
 }
